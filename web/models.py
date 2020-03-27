45e80bd27f081122f6bbfece276760c0b65b5f6f
Sepehr Sohrabi <sepehr0sohrabi@gmail.com>
2020-03-27 18:51:25 +0430

    beautification

--- web/models.py
+++ web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.IntegerField(null=False)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    sahm = models.IntegerField()
+    def __str__(self):
+        return "{}-{}".format(self.name, self.sahm)
