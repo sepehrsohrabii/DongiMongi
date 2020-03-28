1145746238998631fc29ea48860380a668db2eeb
Sepehr Sohrabi <sepehr0sohrabi@gmail.com>
2020-03-28 19:52:13 +0430

    Login Page

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^submit/expense/$', views.submit_expense, name='sumbit_expense'),
     url(r'^submit/person/$', views.submit_person, name='sumbit_person'),
+    url(r'^accounts/register/$', views.register, name='register'),
 ]
